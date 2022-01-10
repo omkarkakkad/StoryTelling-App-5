4566b399e75adfa9ce2b3c60f43d6ab802742d1f
Omkar Kakkad <72384721+omkarkakkad@users.noreply.github.com>
2022-01-10 19:27:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-kheNOQ1UUzJFXKr3JwMN4CjVzLMdGqI',
+  authDomain: 'storytelling-app-2c12c.firebaseapp.com',
+  databaseURL: 'https://storytelling-app-2c12c-default-rtdb.firebaseio.com',
+  projectId: 'storytelling-app-2c12c',
+  storageBucket: 'storytelling-app-2c12c.appspot.com',
+  messagingSenderId: '130657611375',
+  appId: '1:130657611375:web:bd8ef3f75cf371a72ee79a',
+};
+
+const app = initializeApp(firebaseConfig);
